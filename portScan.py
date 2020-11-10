26c98781fde8f382562d9416b45f72793e6496fd
Clesley Oliveira <clesley.oliveira@gruponewway.com.br>
2020-11-09 23:10:30 -0300

    Pyton portscan

---
+++ portScan.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import socket
+import sys
+
+portas = [80, 443, 21, 22, 23, 25, 3306, 3380, 8080, 6380, 27017, 27018, 4369, 5672, 6379]
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def portScanner(portas):
+    for porta in portas:
+        if sock.connect_ex((sys.argv[1], porta)):
+            print("Porta ", porta," fechada")
+        else:
+            print("Porta ", porta, " aberta")
+    print("Concluído")
+sock.close
+
+portScanner(portas)
